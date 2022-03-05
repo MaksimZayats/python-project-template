382d6573207ffaa25172b187a0a435f4ad5c96f0
Maxim <maximzayats1@gmail.com>
2022-03-05 18:21:09 +0300

    style: replace single quotes with double

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,6 @@ import pytest
 from application import utils
 
 
-@pytest.mark.parametrize('a, b, result', ((1, 2, 3), (-1, 1, 0), (2, 0, 2)))
+@pytest.mark.parametrize("a, b, result", ((1, 2, 3), (-1, 1, 0), (2, 0, 2)))
 def test_sum(a: float, b: float, result: float):
     assert utils.sum_over_two_values(a, b) == result
